91877c1ea16cfb80d007f4e51b4321b2e45a03d6
Daniel Sasu <dsasu1@gmail.com>
2025-08-02 22:10:01 -0400

    header redesign

--- src/app/sharedcomponents/headerfooter/header.component.ts
+++ src/app/sharedcomponents/headerfooter/header.component.ts
@@ -1,84 +1,85 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
-import { AppsessionService } from '../../service/appsession.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserSession } from '../../model/usersession.model';
 import { ErrorMessage } from '../../model/utility.model';
+import { AppsessionService } from '../../service/appsession.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-    headerInfoMessage: ErrorMessage = new ErrorMessage();
-    currentUser: UserSession = new UserSession();
-    title: string = "";
-    isLoggedIn: boolean = false;
-    unseenNotificationCount: number = 0;
-    private subsriptionHeaderMsg: any;
-    private subsriptionTitle: any;
-    private subsriptionUserSession: any;
-    private subsriptionIsUserLoggedIn: any;
-    private subsriptionUnviewedNotificationCount: any;
-    constructor(private appsession: AppsessionService) {
-      this.subsriptionUnviewedNotificationCount = this.appsession.currentUnviewdNotificationCountChange.subscribe(value => {
-        this.unseenNotificationCount = value;
-      });
-      this.subsriptionTitle = this.appsession.AppTitleChange.subscribe((value) => {
-            this.title = value
-        });
-      this.subsriptionUserSession =  this.appsession.UserSessionChange.subscribe((value) => {
-            this.currentUser = value
-        });
+  headerInfoMessage: ErrorMessage = new ErrorMessage();
+  currentUser: UserSession = new UserSession();
+  title: string = "";
+  isLoggedIn: boolean = false;
+  unseenNotificationCount: number = 0;
+  private subsriptionHeaderMsg: any;
+  private subsriptionTitle: any;
+  private subsriptionUserSession: any;
+  private subsriptionIsUserLoggedIn: any;
+  private subsriptionUnviewedNotificationCount: any;
+  constructor(private appsession: AppsessionService) {
+    this.subsriptionUnviewedNotificationCount = this.appsession.currentUnviewdNotificationCountChange.subscribe(value => {
+      this.unseenNotificationCount = value;
+    });
+    this.subsriptionTitle = this.appsession.AppTitleChange.subscribe((value) => {
+      this.title = value
+    });
+    this.subsriptionUserSession = this.appsession.UserSessionChange.subscribe((value) => {
+      this.currentUser = value
+    });
 
-      this.subsriptionIsUserLoggedIn =  this.appsession.IsUserLoggedInChange.subscribe(value => {
-            this.isLoggedIn = value;
-        })
+    this.subsriptionIsUserLoggedIn = this.appsession.IsUserLoggedInChange.subscribe(value => {
+      this.isLoggedIn = value;
+    })
+  }
+
+  ngOnInit() {
+    this.subsriptionHeaderMsg = this.appsession.HeaderInfoMsgChange.subscribe(value => {
+      this.headerInfoMessage.isGrowl = true;
+      if (value.isInfo) {
+        this.headerInfoMessage.isInfoMessage();
+      }
+      else if (value.isSuccess) {
+        this.headerInfoMessage.isSuccessMessage();
+      }
+      else if (!value.isSuccess) {
+        this.headerInfoMessage.isErrorMessage();
+      }
+
+
+      this.headerInfoMessage.lifeSpanInMilli = value.lifeSpanInMilli;
+
+      this.headerInfoMessage.add(value.msg);
+
+    });
+
+    let userSess = this.appsession.getCurrentUser();
+    if (userSess != null && userSess.userVM != null && userSess.userVM.lang != null) {
+      this.appsession.setTranslateLang(userSess.userVM.lang);
     }
+  }
 
-    ngOnInit() {
-      this.subsriptionHeaderMsg = this.appsession.HeaderInfoMsgChange.subscribe(value => {
-        this.headerInfoMessage.isGrowl = true;
-        if (value.isInfo) {
-          this.headerInfoMessage.isInfoMessage();
-        }
-        else if (value.isSuccess) {
-          this.headerInfoMessage.isSuccessMessage();
-        }
-        else if (!value.isSuccess) {
-          this.headerInfoMessage.isErrorMessage();
-        }
+  GetImageUrl() {
+    let result = this.appsession.getImageData(this.currentUser.userVM.photoThumbnail, 'image', '../../../assets/img/avatar1.jpg');
+    return result;
 
+  }
 
-        this.headerInfoMessage.lifeSpanInMilli = value.lifeSpanInMilli;
+  routeToNotification() {
+    this.appsession.redirectToRoute("/notification");
+  }
 
-        this.headerInfoMessage.add(value.msg);
-       
-      });
+  ngOnDestroy() {
 
-        let userSess = this.appsession.getCurrentUser();
-        if (userSess != null && userSess.userVM != null && userSess.userVM.lang != null) {
-            this.appsession.setTranslateLang(userSess.userVM.lang);
-        }
-    }
+    this.subsriptionHeaderMsg.unsubscribe();
+    this.subsriptionTitle.unsubscribe();
+    this.subsriptionUserSession.unsubscribe();
+    this.subsriptionIsUserLoggedIn.unsubscribe();
+    this.subsriptionUnviewedNotificationCount.unsubscribe();
 
-    GetImageUrl() {
-      let result = this.appsession.getImageData(this.currentUser.userVM.photoThumbnail, 'image', '../../../assets/img/avatar1.jpg');
-      return result;
-
-    }
-
-    routeToNotification() {
-      this.appsession.redirectToRoute("/notification");
-    }
-
-    ngOnDestroy() {
-
-      this.subsriptionHeaderMsg.unsubscribe();
-      this.subsriptionTitle.unsubscribe();
-      this.subsriptionUserSession.unsubscribe();
-      this.subsriptionIsUserLoggedIn.unsubscribe();
-      this.subsriptionUnviewedNotificationCount.unsubscribe();
-
-    }
+  }
 
 
 }
